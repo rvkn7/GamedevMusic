9f5238840037ca0ddf1b35da0e4ec3b20ac80116
rvkn7 <32423222+rvkn7@users.noreply.github.com>
2017-10-01 00:48:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.Mjk0Nzk4ODAxNDYwMjY0OTYw.DLGr1A.ia3byLl5gSXZ8d4yn3Juywe50bg);
+client.login(process.env.BOT_TOKEN);
